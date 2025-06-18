532b2e1a551e1bfbb68e8247d8e6624e5493e79e
AuraNVC <phongnguyen2012015@gmail.com>
2025-06-18 23:25:51 +0700

    fix blog detail

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -6,14 +6,12 @@ export default function BlogDetail() {
     const { id } = useParams();
 
     const [blogDetail, setBlogDetail] = useState("");
-    const [thumbnail,setThumbnail] = useState("");
 
     useEffect(() => {
         // Lấy chi tiết bài viết nếu có ID trong URL
         const res = API_SERVICE.blogAPI.getById(id);
         res.then((data) => {
             setBlogDetail(data);
-            setThumbnail(`https://localhost:7024/files/blogs//${data.thumbnail}`);
         })
     },[]);
     console.log(blogDetail);
@@ -37,7 +35,7 @@ export default function BlogDetail() {
             <div className="row justify-content-center">
                 <div className="col-12 col-md-10 col-lg-8">
                     <div className="card shadow">
-                        <img src={thumbnail} alt={blogDetail.title} className="card-img-top" style={{ maxHeight: 350, objectFit: "cover" }} />
+                        <img src={`https://localhost:7024/files/blogs//${blogDetail.thumbnail}`} alt={blogDetail.title} className="card-img-top" style={{ maxHeight: 350, objectFit: "cover" }} />
                         <div className="card-body">
                             <div className="mb-2">
                                 <span className="badge bg-info text-dark me-2">{blogDetail?.category}</span>
